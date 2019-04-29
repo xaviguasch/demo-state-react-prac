4ceea06ef81161cbdb457507cc888b7e1e7905e7
xaviguasch <xaviguasch@gmail.com>
2019-04-29 23:06:29 +0200

    added Rando component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Game from './Game'
 import Demo from './Demo'
+import Rando from './Rando'
 import './App.css'
 
 function App() {
   return (
     <div className='App'>
-      <Game />
-      <Demo animal='Bobcat' food='Pineapple' />
+      <Rando maxNum={7} />
     </div>
   )
 }
--- src/Demo.js
+++ src/Demo.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react'
 class Demo extends Component {
   constructor(props) {
     super(props)
-    console.log(this.props)
   }
 
   render() {
